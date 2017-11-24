9aa3355d1838e6233d369fa2bc33dbcb12c12a00
vagrant <vagrant@homestead>
2017-11-24 08:01:28 +0000

    Generated topics

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         \App\Models\User::class  => \App\Policies\UserPolicy::class,
     ];
 
