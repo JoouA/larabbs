10f4c50a575e8c1c2e1d9293bcaba4309290f725
JoouA <tangwtna@163.com>
2017-12-01 22:46:00 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class,'user_id','id');
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -32,7 +32,7 @@
             <div class="topic-body">
                 {!! $topic->body !!}
             </div>
-
+            @can('update',$topic)
             <div class="operate">
                 <hr>
                 <a href="{{ route('topics.edit',$topic->id) }}" class="btn btn-default btn-xs" role="button">
@@ -42,6 +42,7 @@
                     <i class="glyphicon glyphicon-trash"></i>删除
                 </a>
             </div>
+            @endcan
         </div>
     </div>
 </div>
