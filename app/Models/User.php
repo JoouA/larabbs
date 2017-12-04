ba0747e6e72975639d6da564d77c2dae4c2cfe6b
JoouA <tangwtna@163.com>
2017-12-04 12:32:50 +0800

    Admin can manage users

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasRoles;
+
     use Notifiable {
         notify as protected laravelNotify;
     }
@@ -64,5 +65,28 @@ class User extends Authenticatable
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+
+    public function setPasswordAttribute($value)
+    {
+        //如果指的长度等于60，认为是已经做过机密的情况
+        if (strlen($value) != 60){
+            //不等于60，做加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if (!starts_with($path,'http')){
+            //拼接完整的URL
+            $path = config('app.url')."/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
     }
 }
