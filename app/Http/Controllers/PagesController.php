5c23a1b3a25b8f623649357fb112435a9f99ed91
JoouA <tangwtna@163.com>
2017-12-04 16:39:41 +0800

    Admin permissions

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,15 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    public function permissionDenied()
+    {
+        //如果当前用户有访问后台的权限直接跳转
+        if (config('admin.permission')){
+            return redirect(url(config('administrator.uri')),302);
+        }
+
+        //否则使用视图
+        return view('pages.permission_denied');
+    }
 }
--- config/administrator.php
+++ config/administrator.php
@@ -75,13 +75,13 @@ return array(
     'dashboard_view' => '',
 
     // 用来作为后台主页的菜单条目，由 `use_dashboard` 选项决定，菜单指的是 `menu` 选项
-    'home_page' => 'users',
+    'home_page' => 'topics',
 
     // 右上角『返回主站』按钮的链接
     'back_to_site_path' => '/',
 
     // 当选项 `permission` 权限检测不通过时，会重定向用户到此处设置的路径
-    'login_path' => 'login',
+    'login_path' => 'permission-denied',
 
     // 允许在登录成功后使用 Session::get('redirect') 将用户重定向到原本想要访问的后台页面
     'login_redirect_key' => 'redirect',
@@ -92,5 +92,5 @@ return array(
     // 可选的语言，如果不为空，将会在页面顶部显示『选择语言』按钮
     'locales' => [],
 
-    'custom_routes_file' => app_path('Http/routes/administrator.php'),
+//    'custom_routes_file' => app_path('Http/routes/administrator.php'),
 );
