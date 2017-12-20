147db3e694abef30b84e34135b2fd908744b533a
JoouA <tangwtna@163.com>
2017-12-20 19:22:24 +0800

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        if (empty($user->avatar)){
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
